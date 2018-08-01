98dbdf5c840e1f0c7ede79ccb92198cb188d6e33
joshsisley <josh@joshuasisley.com>
2018-07-31 21:53:29 -0400

    initial commit of repo

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: false,
+
+    region: 'us-east-1',
+
+    identityPoolId: 'us-east-1:9c25aa12-6790-48fc-903a-e8c36d6b290d',
+    userPoolId: 'us-east-1_NVM2GgVaL',
+    clientId: '264bme7j9mi9kaa5bus8jg17tq',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
+
